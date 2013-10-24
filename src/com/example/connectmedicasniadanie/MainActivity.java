d7d849a4d959877968511f6728b26bd3e1f19272
Paweł Gajda <pawegio@gmail.com>
2013-10-24 17:13:37 +0200

    dodanie ListView

--- src/com/example/connectmedicasniadanie/MainActivity.java
+++ src/com/example/connectmedicasniadanie/MainActivity.java
@@ -3,13 +3,14 @@ package com.example.connectmedicasniadanie;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // commit for dev branch
-	super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
@@ -19,6 +20,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    private void initListView(ArrayAdapter monumentsArrayAdapter) {
+    	ListView monumentsList = (ListView) findViewById(R.id.listview);
+    	monumentsList.setAdapter(monumentsArrayAdapter);
     }
     
 }
