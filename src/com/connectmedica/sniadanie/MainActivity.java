ed857febc7ecf5b1378b21085ddf7576d39c297e
lczaplicki <lczaplicki@sweter.connectmedica.local>
2013-10-24 15:51:31 +0200

    mala poprawka do Maina - pola

--- src/com/connectmedica/sniadanie/MainActivity.java
+++ src/com/connectmedica/sniadanie/MainActivity.java
@@ -3,14 +3,22 @@ package com.connectmedica.sniadanie;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+    private EditText mEditName;
+    private EditText mEditPlace;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         // commit for dev branch
 	super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mEditName = (EditText) findViewById(R.id.name);
+        mEditPlace = (EditText) findViewById(R.id.place);
+
     }
 
 
