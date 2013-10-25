1566c2c28f82c115b782ac07a5195c668aee7b31
fada <lukasz.czaplicki@gmail.com>
2013-10-25 23:04:47 +0200

    onCreateOptionsMenu

--- src/com/connectmedica/sniadanie/ResultListActivity.java
+++ src/com/connectmedica/sniadanie/ResultListActivity.java
@@ -18,6 +18,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -135,5 +136,10 @@ public class ResultListActivity extends ActionBarActivity {
 				startActivity(i);
 			}
 		});
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
