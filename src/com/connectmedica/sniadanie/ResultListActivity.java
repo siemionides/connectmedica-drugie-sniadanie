0f42119eeef297968b662968af59b615b9468afb
fada <lukasz.czaplicki@gmail.com>
2013-10-26 00:20:41 +0200

    search i porządki

--- src/com/connectmedica/sniadanie/ResultListActivity.java
+++ src/com/connectmedica/sniadanie/ResultListActivity.java
@@ -16,130 +16,148 @@ import com.connectmedica.sniadanie.rest.json.RelicJsonWrapper;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.SearchView.OnCloseListener;
+import android.support.v7.widget.SearchView.OnQueryTextListener;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class ResultListActivity extends ActionBarActivity {
+public class ResultListActivity extends ActionBarActivity implements OnQueryTextListener, OnCloseListener {
+
+    private RelicAdapter mAdapter;
+    private SearchView   mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_list);
-        
-        String[] fakeMonumentsList = new String[] {
-        		"Zabytek 1",
-        		"Zabytek 2",
-        		"Zabytek 3",
-        		"Zabytek 4",
-        		"Zabytek 5",
-        		"Zabytek 6",
-        		"Zabytek 7",
-        		"Zabytek 8",
+
+        String[] fakeMonumentsList = new String[] { "Zabytek 1", "Zabytek 2", "Zabytek 3", "Zabytek 4", "Zabytek 5",
+                "Zabytek 6", "Zabytek 7", "Zabytek 8",
 
         };
-        
+
         Bundle b = getIntent().getExtras();
-        if(b != null)
-        	performApiQuery (b);
-        
+        if (b != null)
+            performApiQuery(b);
 
-        
-
-        
-        
     }
-    
-    private void performApiQuery( Bundle b){
-    	
+
+    private void performApiQuery(Bundle b) {
+
         String relicName = b.getString(MainActivity.KEY_RELIC_NAME);
         String relicPlace = b.getString(MainActivity.KEY_RELIC_PLACE);
         String relicFrom = b.getString(MainActivity.KEY_RELIC_FROM);
         String relicTo = b.getString(MainActivity.KEY_RELIC_TO);
-        
-        
-    	Callback<RelicJsonWrapper> cb = new Callback<RelicJsonWrapper>() {
 
-			@Override
-			public void failure(RetrofitError arg0) {
-				Log.d("connectmedica", "failure");
-				
-			}
+        Callback<RelicJsonWrapper> cb = new Callback<RelicJsonWrapper>() {
 
-			@Override
-			public void success(RelicJsonWrapper arg0, Response arg1) {
-				Log.d("connectmedica", "success");
-				
-				Toast.makeText(getApplicationContext(), "Fetched " + arg0.relics.size() + " relics!", Toast.LENGTH_LONG).show();
-				
-				ArrayList<String> relicNames = new ArrayList<String>();
-				for (RelicJson relic : arg0.relics){
-//					Log.d("zabytek", relic.toString());
-					relicNames.add( relic.identification);
-					for (PhotoJson photo : relic.photos){
-						if (photo.file.maxi != null) Log.d("photo maxi ", photo.file.maxi.url);
-						if (photo.file.mini != null) Log.d("photo mini", photo.file.mini.url);
-						// dodaj do tego urla host http://otwartezabytki.pl/
-//						/system/uploads/photo/file/1010/icon_IMG_0627.JPG
+            @Override
+            public void failure(RetrofitError arg0) {
+                Log.d("connectmedica", "failure");
 
-						
+            }
 
-					}
-				}
-				
-				
-		        /* STARY ADAPTER  
-		        ArrayAdapter<String> adapter = new ArrayAdapter<String>( ResultListActivity.this,
-		        		android.R.layout.simple_list_item_1, relicNames );
-		        initListView(adapter);
-				*/
-				
-				RelicJson[] DATA = new RelicJson[]{};
-				RelicJson[] data = arg0.relics.toArray(DATA);
-		        
-				// NOWY ADAPTER
-				RelicAdapter adapter = new RelicAdapter(ResultListActivity.this, 
-						R.layout.list_row, data);
-				initNewListView(adapter, data);
-				
-				
-			}
-		};
-		
-		OtwarteZabytkiClient.getInstance().getSideEffects(relicPlace, relicName, relicFrom, relicTo, cb);
+            @Override
+            public void success(RelicJsonWrapper arg0, Response arg1) {
+                Log.d("connectmedica", "success");
+
+                Toast.makeText(getApplicationContext(), "Fetched " + arg0.relics.size() + " relics!", Toast.LENGTH_LONG)
+                        .show();
+
+                ArrayList<String> relicNames = new ArrayList<String>();
+                for (RelicJson relic : arg0.relics) {
+                    // Log.d("zabytek", relic.toString());
+                    relicNames.add(relic.identification);
+                    for (PhotoJson photo : relic.photos) {
+                        if (photo.file.maxi != null)
+                            Log.d("photo maxi ", photo.file.maxi.url);
+                        if (photo.file.mini != null)
+                            Log.d("photo mini", photo.file.mini.url);
+                        // dodaj do tego urla host http://otwartezabytki.pl/
+                        // /system/uploads/photo/file/1010/icon_IMG_0627.JPG
+
+                    }
+                }
+
+                /*
+                 * STARY ADAPTER
+                 * ArrayAdapter<String> adapter = new ArrayAdapter<String>( ResultListActivity.this,
+                 * android.R.layout.simple_list_item_1, relicNames );
+                 * initListView(adapter);
+                 */
+
+                RelicJson[] DATA = new RelicJson[] {};
+                RelicJson[] data = arg0.relics.toArray(DATA);
+
+                // NOWY ADAPTER
+                RelicAdapter adapter = mAdapter = new RelicAdapter(ResultListActivity.this, R.layout.list_row, data);
+                initNewListView(adapter, data);
+
+            }
+        };
+
+        OtwarteZabytkiClient.getInstance().getSideEffects(relicPlace, relicName, relicFrom, relicTo, cb);
     }
-    
+
     private void initListView(ArrayAdapter monumentsArrayAdapter) {
-    	ListView monumentsList = (ListView) findViewById(R.id.listview);
-    	monumentsList.setAdapter(monumentsArrayAdapter);
+        ListView monumentsList = (ListView) findViewById(R.id.listview);
+        monumentsList.setAdapter(monumentsArrayAdapter);
     }
-    
-    private void initNewListView(RelicAdapter relicAdapter, final RelicJson[] data) {
-    	ListView relicsList = (ListView) findViewById(R.id.listview);
-    	relicsList.setAdapter(relicAdapter);
-    	relicsList.setOnItemClickListener(new OnItemClickListener() {
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position,
-					long id) {
-				Intent i = new Intent(ResultListActivity.this, DetailsActivity.class);
-				i.putExtra("image", "http://otwartezabytki.pl/" + data[position].photos.get(0).file.maxi.url);
-				i.putExtra(MainActivity.KEY_RELIC_NAME, data[position].identification);
-				i.putExtra(MainActivity.KEY_RELIC_PLACE, data[position].place_name);
-				i.putExtra(MainActivity.KEY_RELIC_FROM, data[position].dating_of_obj);
-				startActivity(i);
-			}
-		});
+    private void initNewListView(RelicAdapter relicAdapter, final RelicJson[] data) {
+        ListView relicsList = (ListView) findViewById(R.id.listview);
+        relicsList.setAdapter(relicAdapter);
+        relicsList.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent i = new Intent(ResultListActivity.this, DetailsActivity.class);
+                i.putExtra("image", "http://otwartezabytki.pl/" + data[position].photos.get(0).file.maxi.url);
+                i.putExtra(MainActivity.KEY_RELIC_NAME, data[position].identification);
+                i.putExtra(MainActivity.KEY_RELIC_PLACE, data[position].place_name);
+                i.putExtra(MainActivity.KEY_RELIC_FROM, data[position].dating_of_obj);
+                startActivity(i);
+            }
+        });
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.result_list, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        mSearchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        mSearchView.setOnQueryTextListener(this);
+        mSearchView.setOnCloseListener(this);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        if (mAdapter != null) {
+            mAdapter.getFilter().filter(query);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String arg0) {
+        return true;
+    }
+
+    @Override
+    public boolean onClose() {
+        mAdapter.getFilter().filter("");
+        return true;
     }
 }
--- src/com/connectmedica/sniadanie/adapter/RelicAdapter.java
+++ src/com/connectmedica/sniadanie/adapter/RelicAdapter.java
@@ -1,30 +1,70 @@
 package com.connectmedica.sniadanie.adapter;
 
-import com.androidquery.AQuery;
-import com.connectmedica.sniadanie.R;
-import com.connectmedica.sniadanie.rest.json.RelicJson;
+import java.util.ArrayList;
+import java.util.List;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;	
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Filter;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.androidquery.AQuery;
+import com.connectmedica.sniadanie.R;
+import com.connectmedica.sniadanie.rest.json.RelicJson;
 
 public class RelicAdapter extends ArrayAdapter<RelicJson> {
 	
 	private AQuery aq;
 	private Context mContext;
-	private RelicJson[] mData;
+	private RelicJson[] mData, mFilterBaseData;
 	private int mRowResId;
+	
+	private Object mLock = new Object();
 	
 	public RelicAdapter(Context context, int rowResId, RelicJson[] data) {
 		super(context, rowResId, data);
 		mContext = context;
 		mRowResId = rowResId;
-		mData = data;
+		mData = mFilterBaseData = data;
 		aq = new AQuery(mContext);
+	}
+	
+	@Override
+	public Filter getFilter() {
+	    return new Filter() {
+            
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults results) {
+                mData = (RelicJson[]) results.values;
+                RelicAdapter.this.notifyDataSetChanged();
+            }
+            
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                synchronized (mLock) {
+                    
+                List<RelicJson> filteredList = new ArrayList<RelicJson>();
+                for (RelicJson  rj : mFilterBaseData) {
+                    if (rj.identification.toLowerCase().contains(constraint.toString().toLowerCase()))
+                        filteredList.add(rj);
+                }
+                RelicJson[] resArray = new RelicJson[]{};
+                FilterResults results = new FilterResults();
+                results.values = filteredList.toArray(resArray);
+                return results;
+                }
+            }
+        };
+	}
+	
+	@Override
+	public int getCount() {
+	    if (mData == null) return 0;
+	    return mData.length;
 	}
 	
 	@Override
